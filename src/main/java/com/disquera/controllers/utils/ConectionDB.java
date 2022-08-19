c93706e626d8783fe56327afbbdbf29f28f550ee
Daniel Roa <lion35528@gmail.com>
2022-08-18 21:46:20 -0500

    Add connection to database disqueradb

---
+++ src/main/java/com/disquera/controllers/utils/ConectionDB.java
@@ -0,0 +1,54 @@
+package com.disquera.controllers.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectionDB {
+
+    // Instantiate the variables to connect database
+    private final String driver;
+    private final String user;
+    private final String password;
+    private final String dataBase;
+    private final String urlDB;
+
+
+
+    // Instantiate object connection
+    private Connection connection;
+
+    public ConectionDB(){
+        driver = "com.mysql.cj.jdbc.Driver";
+        user = "root";
+        password = "";
+        dataBase = "disqueradb";
+        urlDB = "jdbc:mysql://localhost:3306/" + dataBase;
+
+        // Connect to database
+        try {
+            Class.forName(driver).newInstance();
+            connection = DriverManager.getConnection(urlDB, user,password);
+            System.out.println("Conexion ok!");
+
+        } catch (Exception e) {
+            System.out.println("Error al conectarse" + e);
+
+        }
+    }
+
+    public Connection cerrarConexion() throws SQLException {
+        connection.close();
+        connection = null;
+        return connection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static void main (String [] args){
+        new ConectionDB();
+    }
+
+}
