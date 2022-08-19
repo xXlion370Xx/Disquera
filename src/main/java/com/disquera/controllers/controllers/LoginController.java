2878cd50b140e8db2723a3388b617391482a6b16
Daniel Roa <lion35528@gmail.com>
2022-08-19 01:08:34 -0500

    Finished login controller, issues : itdon´t have a rol recognition

--- src/main/java/com/disquera/controllers/controllers/LoginController.java
+++ src/main/java/com/disquera/controllers/controllers/LoginController.java
@@ -1,27 +1,60 @@
 package com.disquera.controllers.controllers;
 
-import java.io.*;
-
+import com.disquera.controllers.models.LoginModelDAO;
+import com.disquera.controllers.models.LoginModelVO;
+import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "LoginController", value = "/Login")
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static java.lang.Integer.parseInt;
+
+@WebServlet(name = "LoginController", value = {"/Login"})
+
 public class LoginController extends HttpServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Request HTTP post from views
         String user = request.getParameter("user");
         String password = request.getParameter("password");
+        int option = parseInt(request.getParameter("option"));
 
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + user + " " + password + "</h1>");
-        out.println("</body></html>");
+        // Instantiate objects models
+        LoginModelVO loginModelVO = new LoginModelVO(user, password);
+        LoginModelDAO loginModelDAO = new LoginModelDAO(loginModelVO);
+
+        boolean validateLogin = false;
+        // Management data
+
+        switch (option){
+            case 1:
+                try {
+                    validateLogin = loginModelDAO.validateLogin();
+                    if (validateLogin){
+                        request.getRequestDispatcher("artista.jsp").forward(request, response);
+                    }
+                    request.setAttribute("mensajeError", "Error, intentalo nuevamente");
+                    request.getRequestDispatcher("index.jsp").forward(request, response);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            default:
+                request.setAttribute("mensajeError", "Algo salio mal");
+                request.getRequestDispatcher("index.jsp").forward(request,response);
+        }
+
+
     }
 
-    public void destroy() {
-    }
 }
