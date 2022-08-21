960a3f0e2c6b6e9155d091a760066fec99b11317
Daniel Roa <lion35528@gmail.com>
2022-08-21 13:37:09 -0500

    Add new methods that list user data and method validate de existence of that user

--- src/main/java/com/disquera/controllers/models/UserModelDAO.java
+++ src/main/java/com/disquera/controllers/models/UserModelDAO.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,9 +31,7 @@ public class UserModelDAO extends ConnectionDB {
         }
     }
 
-    // Login method return an array of 2 values, first is the user rol and second value of user state
-    public String[] validateLogin() throws SQLException {
-        String[] resultQuery = new String[2];
+    public boolean validateLogin(){
         try{
             // Do prepare statement to get de result query
             sql = "SELECT * FROM usuario WHERE nombreUsuario = ? AND passwordUsuario = ?";
@@ -42,15 +41,33 @@ public class UserModelDAO extends ConnectionDB {
             // Execute query
             ResultSet resultSet = query.executeQuery();
 
-            if (resultSet.next()) {
-                resultQuery[0] = resultSet.getString(4);
-                resultQuery[1] = resultSet.getString(5);
+            return resultSet.next();
 
-                this.cerrarConexion();
-                return resultQuery;
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Login method return information about user
+    public ArrayList<UserModelVO> listUserData() throws SQLException {
+        ArrayList<UserModelVO> userData = new ArrayList<>();
+        try{
+            // Do prepare statement to get de result query
+            sql = "SELECT * FROM usuario WHERE nombreUsuario = ? AND passwordUsuario = ?";
+            query = connection.prepareStatement(sql);
+            query.setString(1, user);
+            query.setString(2, password);
+            // Execute query
+            ResultSet resultSet = query.executeQuery();
+
+            while (resultSet.next()) {
+
+                UserModelVO userModelVO = new UserModelVO(resultSet.getString(1), resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5));
+
+                userData.add(userModelVO);
             }
-
-            return null;
+            this.cerrarConexion();
+            return userData;
 
         }catch (SQLException e) {
             throw new RuntimeException(e);
