58d581fbb1f6046ead26f5927d02c17c273429d2
Daniel Roa <lion35528@gmail.com>
2022-08-21 18:43:12 -0500

    Add method consult music of artist

--- src/main/java/com/disquera/controllers/models/ArtistModelDAO.java
+++ src/main/java/com/disquera/controllers/models/ArtistModelDAO.java
@@ -3,11 +3,19 @@ package com.disquera.controllers.models;
 import com.disquera.controllers.utils.ConnectionDB;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ArtistModelDAO extends ConnectionDB {
 
     // Instantiate variables
     Connection connection;
+    String sql;
+    PreparedStatement query;
     private String id;
     private String numDoc;
     private String typeDoc;
@@ -36,4 +44,24 @@ public class ArtistModelDAO extends ConnectionDB {
     }
 
     // Create method consult songs
+    public ArrayList<MusicVO> consultMusic(){
+        ArrayList<MusicVO> musicList = new ArrayList<>();
+        try {
+            // connect to dabase
+            connection = this.getConnection();
+            sql = "SELECT cancion.idCancion, cancion.nombreCancion, cancion.fechaGrabacion, cancion.duracionCancion, cancion.idAlbumFK, cancion.estadoCancion FROM cancion INNER JOIN album a on cancion.idAlbumFK = a.idAlbum INNER JOIN artista a2 on a.idArtistaFK = a2.idArtista INNER JOIN usuario u on a2.idUsuarioFK = u.idUsuario WHERE idUsuario = ?;";
+            query = connection.prepareStatement(sql);
+            query.setString(1, userId);
+            ResultSet resultSet = query.executeQuery();
+            while (resultSet.next()){
+                MusicVO musicVO = new MusicVO(resultSet.getString("idCancion"), resultSet.getString("nombreCancion"), resultSet.getString("fechaGrabacion"), resultSet.getString("duracionCancion"), resultSet.getString("idAlbumFK"), resultSet.getString("estadoCancion"));
+                musicList.add(musicVO);
+            }
+
+            return musicList;
+        }catch (SQLException e){
+            Logger.getLogger(MusicVO.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        }
+    }
 }
