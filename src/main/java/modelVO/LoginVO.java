ac5a6e2321b10832b02f8c508d912b650c13e0dc
MelannyJutinico <melannyjutinico@gmail.com>
2022-08-21 16:51:44 -0500

    creacion del loginVO

---
+++ src/main/java/modelVO/LoginVO.java
@@ -0,0 +1,60 @@
+package modelVO;
+
+public class LoginVO {
+    //encapsular los datos a recibir
+    private String idUsuario, nombreUsuario, passwordUsuario, rolUsuario, estadoUsuario;
+
+
+    //creacion de constructor lleno y vacio
+    public LoginVO(String nombreUsuario, String passwordUsuario, String rolUsuario, String estadoUsuario, String idUsuario) {
+        this.nombreUsuario = nombreUsuario;
+        this.passwordUsuario = passwordUsuario;
+        this.rolUsuario = rolUsuario;
+        this.estadoUsuario = estadoUsuario;
+        this.idUsuario = idUsuario;
+    }
+
+    public LoginVO() {
+    }
+
+    //metodos Getter and Setter
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPasswordUsuario() {
+        return passwordUsuario;
+    }
+
+    public void setPasswordUsuario(String passwordUsuario) {
+        this.passwordUsuario = passwordUsuario;
+    }
+
+    public String getRolUsuario() {
+        return rolUsuario;
+    }
+
+    public void setRolUsuario(String rolUsuario) {
+        this.rolUsuario = rolUsuario;
+    }
+
+    public String getEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(String estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
