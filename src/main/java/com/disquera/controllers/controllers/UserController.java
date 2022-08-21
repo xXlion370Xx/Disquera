b5fbdd101fdef7917ef4b1a986f439b7f73d38cc
Daniel Roa <lion35528@gmail.com>
2022-08-21 13:38:24 -0500

    Add validation if the user does not exist and setting the user data

--- src/main/java/com/disquera/controllers/controllers/UserController.java
+++ src/main/java/com/disquera/controllers/controllers/UserController.java
@@ -29,37 +29,52 @@ public class UserController extends HttpServlet {
         UserModelVO userModelVO = new UserModelVO(user, password);
         UserModelDAO userModelDAO = new UserModelDAO(userModelVO);
 
-        String[] validateLogin;
         // Management data
-
             try {
+
+                if (userModelDAO.validateLogin()){
+
                 // Call model method login to validate the user
-                validateLogin = userModelDAO.validateLogin();
+                for (UserModelVO userData : userModelDAO.listUserData()) {
+                    userModelVO.setId(userData.getId());
+                    userModelVO.setUser(userData.getUser());
+                    userModelVO.setPassword(userData.getPassword());
+                    userModelVO.setRol(userData.getRol());
+                    userModelVO.setStatus(userData.getStatus());
+                }
+                    if(user.equals(userModelVO.getUser()) && password.equals(userModelVO.getPassword())){
 
-                if (!(validateLogin == null)) {
-                    //Validate if the user is active
+                        //Validate if the user is active
+                        if (userModelVO.getRol().equals("Artista") && userModelVO.getStatus().equals("inactivo")) {
 
-                    if(validateLogin[0].equals("Artista") && validateLogin[1].equals("activo")){
+                            request.setAttribute("mensajeError", "Usuario inactivo, por favor contacte con un administrador");
+                            request.getRequestDispatcher("index.jsp").forward(request, response);
 
-                        request.getRequestDispatcher("Artista/artista.jsp").forward(request, response);
+                        }else if(userModelVO.getRol().equals("Artista") && userModelVO.getStatus().equals("activo")){
 
-                    } else if(validateLogin[0].equals("Artista") && validateLogin[1].equals("inactivo")){
+                            HttpSession session = request.getSession(true);
+                            session.setAttribute("userData", userModelVO);
+                            request.getRequestDispatcher("Artista/artista.jsp").forward(request, response);
 
-                        request.setAttribute("mensajeError", "Usuario inactivo, por favor contacte con un administrador");
+                        }// Validate the admin
+                        else if(userModelVO.getRol().equals("Administrador")){
+
+                            HttpSession session = request.getSession(true);
+                            session.setAttribute("userData", userModelVO);
+                            request.getRequestDispatcher("Administrador/administrador.jsp").forward(request, response);
+
+                        }
+
+                    }else {
+
+                        request.setAttribute("mensajeError", "Usuario o contraseña incorrectos");
                         request.getRequestDispatcher("index.jsp").forward(request, response);
 
-                    }// Validate the admin
-                    else if(validateLogin[0].equals("Administrador")){
-
-                        request.getRequestDispatcher("Administrador/administrador.jsp").forward(request, response);
-
                     }
-                } else {
-
-                    request.setAttribute("mensajeError", "Datos incorrectos");
-                    request.getRequestDispatcher("index.jsp").forward(request, response);
-
                 }
+                request.setAttribute("mensajeError", "Usuario no existe");
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+
 
             } catch (SQLException e) {
                 throw new RuntimeException(e);
