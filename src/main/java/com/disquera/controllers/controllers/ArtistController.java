e3fb197af9ec9e95427b41118e84fc2285503566
Daniel Roa <lion35528@gmail.com>
2022-08-21 18:42:00 -0500

    Add management http get request view music

--- src/main/java/com/disquera/controllers/controllers/ArtistController.java
+++ src/main/java/com/disquera/controllers/controllers/ArtistController.java
@@ -1,21 +1,33 @@
 package com.disquera.controllers.controllers;
 
+import com.disquera.controllers.models.ArtistModelDAO;
+import com.disquera.controllers.models.ArtistModelVO;
+import com.disquera.controllers.models.MusicVO;
+import com.disquera.controllers.models.UserModelVO;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.util.ArrayList;
 
 @WebServlet(name = "ArtistController", value = "/Artist")
 public class ArtistController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String view = request.getParameter("view");
+        HttpSession session = request.getSession();
+        UserModelVO userModelVO = (UserModelVO) session.getAttribute("userData");
 
         switch (view){
             case "music":
+                ArtistModelVO artistModelVO = new ArtistModelVO(userModelVO.getUser(), userModelVO.getPassword(), userModelVO.getId());
+                ArtistModelDAO artistModelDAO = new ArtistModelDAO(artistModelVO);
+                ArrayList<MusicVO> listMusic =  artistModelDAO.consultMusic();
 
+                request.setAttribute("listMusic", listMusic);
+                request.getRequestDispatcher("Artista/music.jsp").forward(request, response);
                 break;
             case "album":
                 break;
