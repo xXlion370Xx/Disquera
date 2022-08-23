0f66ee9c25a5110fe8043513e87be8bc05912e0a
JJ007 <jamespopeye10@gmail.com>
2022-08-23 09:14:22 -0500

    Add sessions controller

---
+++ src/main/java/Controller/Sesiones.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebServlet(name = "Sesiones", urlPatterns = {"/Sesiones"})
+public class Sesiones extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession miSesion = request.getSession();
+        miSesion.removeAttribute("datosUsuario");
+        miSesion.invalidate();
+        request.getRequestDispatcher("Album.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
